67c2e174b5809ed1e05cdb9833fe928e1308f514
XMarlinX <41376694+XMarlinX@users.noreply.github.com>
2018-09-25 16:23:37 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix() and CacheMean() work together to initialize a vector, store it in a cache, and determine the inverse of a vector if it has not already been determined. 
+## 
 
-## Write a short comment describing this function
+## makeVector instantiates a vector that stores two assessor functions and two mutator functions.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+                {mean <- NULL
+                set <- function(y)
+                   {x<<y}
+                x <<- y
+                m <<- NULL
+                }
+                get <- function () x
+                setmean <- function(mean) m <<- mean
+                getmean <- function () m
+                return (function_list <- c( set = set(),
+                        get = get,
+                        setmean = setmean,
+                        getmean = getmean
+                        )  )
 }
 
 
-## Write a short comment describing this function
+## cacheMean determines the inverse of a matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheMean <- function(x)
+{   m <<- x$getmean()
+    if (!is.null(m))
+         {
+         print("getting cached data")
+         return m
+         }
+         data <- x$get()
+         m <- mean(data)
+         x$setmean(m)
+         inverse_x <- solve(x)
+         print (inverse_x)
+        
 }
